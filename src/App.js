4200796ccf665661e3dd184083598168108b4e8b
jbcortez <justin.b.cortez@gmail.com>
2021-06-15 21:48:35 -0700

    mobile vh adjustment

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import Home from './components/Home';
 
 function App() {
   useEffect(() => {
-    const appHeight = () => {
-      const doc = document.documentElement;
-      doc.style.setProperty('--app-height', `${window.innerHeight}px`);
-    };
-    window.addEventListener('resize', appHeight);
-    appHeight();
+    window.addEventListener('resize', () => {
+      // We execute the same script as before
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
   }, []);
 
   return (
